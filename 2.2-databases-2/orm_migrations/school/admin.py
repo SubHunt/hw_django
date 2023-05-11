292c98fb3211e0516541b9f3f0fc5756d7792f25
Sergey Matofonov <phaeton.kz@gmail.com>
2023-05-11 10:29:12 +0300

    orm-migrate

--- 2.2-databases-2/orm_migrations/school/admin.py
+++ 2.2-databases-2/orm_migrations/school/admin.py
@@ -5,9 +5,12 @@ from .models import Student, Teacher
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    pass
+    filter_horizontal = ['teachers']
 
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name', 'subject']
+
+# admin.site.register(Teacher, TeacherAdmin)
+# admin.site.register(Student, StudentAdmin)
--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -6,10 +6,12 @@ from .models import Student
 
 def students_list(request):
     template = 'school/students_list.html'
-    context = {}
-
+    student = Student.objects.order_by('group', 'name')
+    context = {'object_list': student,
+              # 'object_list2': student_2,
+              }
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
+    # ordering = 'group'
 
     return render(request, template, context)
