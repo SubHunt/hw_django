a1f93995c0c1d0fa4b8aa66556c849c5249be22e
Sergey Matofonov <phaeton.kz@gmail.com>
2023-05-12 23:33:57 +0300

    Add ORM_migrate

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,36 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from .models import Article, Scope, Tag
 
-from .models import Article
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        is_main = False
+        for form in self.forms:
+            if form.cleaned_data and form.cleaned_data['is_main']:
+                is_main = True
+                count += 1
+        if not is_main:
+            raise ValidationError('Укажите основной раздел')
+        elif count > 1:
+            raise ValidationError('Основным может быть только один раздел')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+    extra = 3
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title', 'published_at']
+    inlines = [ScopeInline, ]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
