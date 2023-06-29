7e5c4f1186c3281359a020115e449aab5da6ba18
Sergey Matofonov <phaeton.kz@gmail.com>
2023-06-29 12:43:32 +0300

    hw_crud

--- 3.2-crud/stocks_products/logistic/admin.py
+++ 3.2-crud/stocks_products/logistic/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Product
 # Register your models here.
+@admin.register(Product)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description']
+    list_filter = ['title']
